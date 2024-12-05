7fffff323e2577bbba2e698657d09e7a326ccaea
fabiomarques1 <94563590+fabiomarques1@users.noreply.github.com>
2024-12-05 16:29:50 -0300

    Add files via upload

--- IntroPOO/src/intropoo/IntroPOO.java
+++ IntroPOO/src/intropoo/IntroPOO.java
@@ -14,109 +14,21 @@ e exibir os cálculos
 */
 public class IntroPOO {
 
+    static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
     public static void main(String[] args) {
         ArrayList<Pessoa> pessoas = new ArrayList<>();
-        
-        loopPrincipal:
         while (true) {
-            //Instanciando o teclado
-            String input;
-            //Instanciando pessoa
-            Pessoa p = new Pessoa();
-            //Pedindo CPF e Nome
-            boolean erro = true;
-            do {
-                try {
-                    input = JOptionPane.showInputDialog("Informe o CPF ou digite -1 para SAIR: ");
-                    if (input.equals("-1")) {
-                        break loopPrincipal;
-                    }
-                    if (input.length() != 11) {
-                        throw new Exception("CPF precisa ter 11 dígitos!");
-                    }
-                    p.setCpf(Long.parseLong(input));
-                    erro = false;
-                } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(null, "CPF Inválido");
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, ex.getMessage());
-                }
-            } while (erro);
-
-            erro = true;
-            do {
-                try {
-                    input = JOptionPane.showInputDialog("Informe o Nome: ");
-                    if (input.trim().isEmpty()) {
-                        throw new Exception("Nome é obrigatório");
-                    }
-                    p.setNome(input);
-                    erro = false;
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, ex.getMessage());
-                }
-            } while (erro);
-            //Pedindo a Data de Nascimento
-            erro = true;
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-            sdf.setLenient(false);
-            do {
-                String dt = JOptionPane.showInputDialog("Informe a Data de Nascimento: ");
-                try {
-                    Date data = sdf.parse(dt);
-                    Calendar dataNasc = Calendar.getInstance();
-                    dataNasc.setTime(data);
-                    Calendar hoje = Calendar.getInstance();
-                    hoje.getTime();
-                    if (hoje.before(dataNasc)) {
-                        throw new Exception("Data Nascimento não pode ser posterior a data de hoje!");
-                    }
-                    p.setDataNascimento(data);
-                    erro = false;
-                } catch (ParseException ex) {
-                    JOptionPane.showMessageDialog(null, "Data Inválida!");
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, ex.getMessage());
-                }
-            } while (erro);
-            //Pedindo a Qtde de Filhos
-            erro = true;
-            do {
-                try {
-                    input = JOptionPane.showInputDialog("Informe a qtde de filhos: ");
-                    p.setQtdeFilhos(Integer.parseInt(input));
-                    erro = false;
-                } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(null, "Qtde Inválida");
-                }
-            } while (erro);
-            //Pedindo a Qtde de Horas
-            erro = true;
-            do {
-                try {
-                    float hora = Float.parseFloat(JOptionPane.showInputDialog("Informe a qtde de horas: "));
-                    p.setHorasTrabalhadas(hora);
-                    erro = false;
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, "Formato de horas inválido");
-                }
-            } while (erro);
-            //Pedindo o valor hora
-            erro = true;
-            do {
-                try {
-                    input = JOptionPane.showInputDialog("Informe o valor hora: ");
-                    p.setValorHora(Float.parseFloat(input));
-                    erro = false;
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(null, "Formato de valor hora inválido");
-                }
-            } while (erro);
-
+            Pessoa p = cadastrarPessoa();
+            if (p == null) {
+                break;
+            }
             //Cálculo e exibição dos resultados
             float valorAReceber = p.getValorHora() * p.getHorasTrabalhadas();
             float bonus = p.calcularBonus();
-            String mensagem = "CPF: " + p.getCpf();
+            String cpf = String.valueOf(p.getCpf());
+            cpf = cpf.substring(0,3) + "." + cpf.substring(3,6) + "." + cpf.substring(6,9) + "-" + cpf.substring(9,11);
+            String mensagem = "CPF: " + cpf;
             mensagem += "\nNome: " + p.getNome();
             mensagem += "\nData de Nascimento: " + sdf.format(p.getDataNascimento());
             mensagem += "\nIdade: " + p.calcularIdade();
@@ -127,12 +39,103 @@ public class IntroPOO {
             JOptionPane.showMessageDialog(null, mensagem);
             pessoas.add(p);
         }
-        
+
         String m = "Pessas cadastradas:";
-        for (Pessoa p: pessoas) {
+        for (Pessoa p : pessoas) {
             m += "\n" + p.getNome();
         }
         JOptionPane.showMessageDialog(null, m);
     }
+
+    public static Pessoa cadastrarPessoa() {
+        //Instanciando o teclado
+        String input;
+        //Instanciando pessoa
+        Pessoa p = new Pessoa();
+        //Pedindo CPF   
+        while (true) {
+            try {
+                input = JOptionPane.showInputDialog("Informe o CPF ou digite -1 para SAIR: ");
+                if (input.equals("-1")) {
+                    return null;
+                }
+                if (input.length() != 11) {
+                    throw new Exception("CPF precisa ter 11 dígitos!");
+                }
+                p.setCpf(Long.parseLong(input));
+                break;
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "CPF Inválido");
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }
+        //Pedindo o Nome
+        while (true) {
+            try {
+                input = JOptionPane.showInputDialog("Informe o Nome: ");
+                if (input.trim().isEmpty()) {
+                    throw new Exception("Nome é obrigatório");
+                }
+                p.setNome(input);
+                break;
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }
+        //Pedindo a Data de Nascimento
+        while (true) {
+            try {
+                sdf.setLenient(false);
+                String dt = JOptionPane.showInputDialog("Informe a Data de Nascimento: ");
+                Date data = sdf.parse(dt);
+                Calendar dataNasc = Calendar.getInstance();
+                dataNasc.setTime(data);
+                Calendar hoje = Calendar.getInstance();
+                hoje.getTime();
+                if (hoje.before(dataNasc)) {
+                    throw new Exception("Data Nascimento não pode ser posterior a data de hoje!");
+                }
+                p.setDataNascimento(data);
+                break;
+            } catch (ParseException ex) {
+                JOptionPane.showMessageDialog(null, "Data Inválida!");
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }
+        //Pedindo a Qtde de Filhos
+        while (true) {
+            try {
+                input = JOptionPane.showInputDialog("Informe a qtde de filhos: ");
+                p.setQtdeFilhos(Integer.parseInt(input));
+                break;
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Qtde Inválida");
+            }
+        }
+        //Pedindo a Qtde de Horas
+        while (true) {
+            try {
+                float hora = Float.parseFloat(JOptionPane.showInputDialog("Informe a qtde de horas: "));
+                p.setHorasTrabalhadas(hora);
+                break;
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Formato de horas inválido");
+            }
+        }
+        //Pedindo o valor hora
+        while (true) {
+            try {
+                input = JOptionPane.showInputDialog("Informe o valor hora: ");
+                p.setValorHora(Float.parseFloat(input));
+                break;
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Formato de valor hora inválido");
+            }
+        }
+        return p;
+    }
+
 
 }
